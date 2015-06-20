1630e3ac924d09f974e84a4111b0ebd2dd12b1d8
colemantung <cmtkusc@gmail.com>
2015-06-19 17:24:49 -0700

    Set up connection to memcache server.

--- connect.py
+++ connect.py
@@ -1,1 +1,11 @@
 __author__ = 'ColemanTung'
+
+
+import pylibmc
+
+mc = pylibmc.Client(["127.0.0.1"])
+
+mc["foo"] = "ABCDEFG"
+print(mc.get("foo"))
+
+#(["127.0.0.1"], binary=True, behaviors={"tcp_nodelay": True, "verify_keys": True, "ketama": True})
